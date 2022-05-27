8f24aa35cf7e9038436da9c1804e3e0cc4274047
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-27 10:20:40 -0700

    Exercise completed!

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/*
+We have a dog that likes to bark. We need to wake up if the dog is barking at night!
+
+Write a method shouldWakeUp that has 2 parameters.
+
+1st parameter should be the type boolean and be named barking, it represents if our dog is currently barking.
+2nd parameter represents the hour of the day, and is of type int with the name hourOfDay and has a valid range of 0-23.
+
+We have to wake up if the dog is barking before 8 or after 22 hours so in that case returns true.
+
+In all other cases return false.
+
+If the hourOfDay parameter is less than 0 or greater than 23 return false.
+
+TIP: Use the if else statement with multiple conditions.
+NOTE: The shouldWakeUp method needs to be defined as public static like we have been doing so far in the course.
+NOTE: Do not add a main method to the solution code.
+ */
+public class Main {
+    public static void main(String[] args) {
+        boolean output = BarkingDog.shouldWakeUp(true,-1);
+        System.out.println(output);
+    }
+}
